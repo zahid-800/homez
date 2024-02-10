3bde0001e1ce503be040d8ad9dd2d76bd7e4c9f2
zahid-800 <154730312+zahid-800@users.noreply.github.com>
2024-02-10 14:45:45 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function navBar(){
+    const navMobile=document.getElementById('mobileShow');
+    navMobile.style.display='flex';
+  }
+
+  function navBarcross(){
+    const navMobile=document.getElementById('mobileShow');
+    navMobile.style.display='none';
+  }
+
